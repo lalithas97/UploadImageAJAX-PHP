7d9ad62326d58a8305f1e3b059f293381f1528b8
lalithas97 <78196574+lalithas97@users.noreply.github.com>
2022-04-01 17:00:42 +0530

    Add files via upload

--- upload.php
+++ upload.php
@@ -1,25 +1,42 @@
 <?php
+include "db.php";
+
+	echo $id;
 
 if(isset($_FILES['file']['name'])){
+	// Getting number of rows
+	$res = mysql_query("select * from imgup");
+	$id=mysql_num_rows($res);
+	$id=$id+1;
+	
 
 	/* Getting file name */
 	$filename = $_FILES['file']['name'];
 
 	/* Location */
+	
+
 	$location = "upload/".$filename;
 	$imageFileType = pathinfo($location,PATHINFO_EXTENSION);
-	$imageFileType = strtolower($imageFileType);
+	$imageFileType_lc = strtolower($imageFileType);
 
 	/* Valid extensions */
-	$valid_extensions = array("jpg","jpeg","png");
+	$allowed_exs = array("jpg","jpeg","png");
 
 	$response = 0;
 	/* Check file extension */
-	if(in_array(strtolower($imageFileType), $valid_extensions)) {
+	
+	if(in_array($imageFileType_lc, $allowed_exs)) {
+		
+	
 	   	/* Upload file */
 	   	if(move_uploaded_file($_FILES['file']['tmp_name'],$location)){
 	     	$response = $location;
 	   	}
+		//    Inserting into database
+		mysql_query("insert into imgup(img) values('$id.$imageFileType_lc')");
+		
+		
 	}
 
 	echo $response;
